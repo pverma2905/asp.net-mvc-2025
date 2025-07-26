069c7ffda075f2fc4903896d1a7a68c28420fa02
Pranav Verma <pverma2905@gmail.com>
2025-07-26 19:41:25 +0530

    second

---
+++ 10ViewToControllerModel/10ViewToControllerModel/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using _10ViewToControllerModel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _10ViewToControllerModel.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowData(Product prod)
+        {
+            ViewBag.Id = prod.prodid;
+            ViewBag.Name = prod.prodname;
+            ViewBag.Qty = prod.prodqty;
+            ViewBag.Price = prod.prodprice;
+            return View();
+
+        }
+    }
+}
---
+++ 12StronglyTypedHelper/12StronglyTypedHelper/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using _12StronglyTypedHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _12StronglyTypedHelper.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Login l = new Login() { userName = "admin", password = "1234" };
+            return View(l);
+        }
+    }
+}
